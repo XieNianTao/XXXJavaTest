74b6390eb211471f9cbb682d74b78795d48425c0
xieniantao <z8oo@sina.com>
2015-09-26 19:51:29 +0800

    no message

---
+++ src/leet/test.java
@@ -0,0 +1,32 @@
+package leet;
+
+import sun.tools.tree.ThisExpression;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		String a="11101011";
+//		String b="111";
+//		addBinary.addBinary(a, b);
+		
+//		pascals.getRow(6);
+//		onebits.hammingWeight(11);
+		
+//		int[] a={1,2,3,4,5,6,7};
+//		int[] b={1,2};
+//		
+//		Rotate.rotate(b, 3);
+		
+		
+//		String s=reverseWords.reverseWords("       ");
+		
+		boolean i=HappyNumber.isHappy(19);
+		 
+		int cca=2;
+
+	}
+	
+}
+
+
